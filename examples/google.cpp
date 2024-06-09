f36397b6e8adcadfcaa0ed8477d7648f8a8112f9
Carlos Menezes <carlos-menezes212@hotmail.com>
2024-06-09 15:29:10 +0100

    chore: remove dead code

--- examples/google.cpp
+++ examples/google.cpp
@@ -1,7 +1,4 @@
 #include "../include/libg203lightsync.h"
-#include <thread>
-
-using namespace std::chrono_literals;
 
 int main() {
     g203_lightsync mouse;
@@ -10,9 +7,6 @@ int main() {
     const std::array<const uint8_t, 3> center = {0xee, 0xb2, 0x11};
     const std::array<const uint8_t, 3> right = {0xd5, 0x0f, 0x25};
 
-    for (uint8_t i = 0; i < 0xFF; i++) {
-        mouse.set_triple({left, center, right});
-        std::this_thread::sleep_for(100ms);
-    }
+    mouse.set_triple({left, center, right});
     return 0;
 }
